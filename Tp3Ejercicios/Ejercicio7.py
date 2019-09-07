90f734b4a947aa2db1dd8e2d3f4ef463673a9f4c
Leandro Cantero <42678932+LeandroCantero@users.noreply.github.com>
2019-09-06 21:35:16 -0300

    2 formas

--- Tp3Ejercicios/Ejercicio7.py
+++ Tp3Ejercicios/Ejercicio7.py
@@ -2,8 +2,13 @@
 
 def decimalABase2(n):
     base2 = 0
-    if n == 1:
-        base2 = 1
+    if n == :
+        base2 = 0
     else:
-        base2 = n % decimalABase2(n//2)
+        base2 = n % 2 + 10 * decimalABase2(n//2)
     return base2
+
+def aBinario(x):
+    if x>1:
+        aBinario(x//2)
+    print (x%2, end='')
