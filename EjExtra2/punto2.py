57fd8007b3ef8a6d7adc49fad6df51bab7983939
Leandro <lcantero18@gmail.com>
2019-09-16 20:44:01 -0300

    tpExtra2

---
+++ EjExtra2/punto2.py
@@ -0,0 +1,24 @@
+#Tp Extra 2
+
+'''
+2)Se quiere implementar una función: distribucion(cadena):
+La misma devuelve un vector de pares con un TAD de dos partes:
+-Letra
+-Ocurrencia
+Es decir, para cada letra de la cadena, se guarda la cantidad de veces que aparece.
+'''
+
+'''
+TDA: Tipo abstracto de datos (Hay que armarlo)
+AYUDAS:
+- Para inicializar un vector cuyos elementos sean TDA que por ejemplo se llama
+‘Letras’ usar:
+vector=np.empty(len(cadena),dtype=Letras)
+-
+-
+Cuando se recorre un vector que no tiene elementos de ese tipo se puede
+preguntar por vector[i]==None
+La solución también es válida si se repiten las letras pero las cantidades son
+correctas.
+'''
+
